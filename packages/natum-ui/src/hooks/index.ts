3cacabd9bc8fd24bd55e789b902927346e387174
jonathanramlie <jonathanramlie@outlook.com>
2026-03-30 01:23:26 +0800

    chore: export new hooks from barrel

--- packages/natum-ui/src/hooks/index.ts
+++ packages/natum-ui/src/hooks/index.ts
@@ -1,5 +1,9 @@
+export { useAnimationState } from "./use-animation-state";
 export { useAnchorPosition } from "./use-anchor-position";
+export { useClickOutside } from "./use-click-outside";
 export { useControllable } from "./use-controllable";
 export { useCountdown } from "./use-countdown";
+export { useEscapeKey } from "./use-escape-key";
 export { useFocusTrap } from "./use-focus-trap";
+export { useScrollLock } from "./use-scroll-lock";
 export { mergeRefs, useMergedRefs } from "./use-merge-refs";
