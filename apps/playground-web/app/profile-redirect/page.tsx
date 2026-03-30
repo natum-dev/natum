ca43ea0a9431389814bcf59c3772d569ca55a44f
jonathanramlie <jonathanramlie@outlook.com>
2026-03-30 16:42:44 +0800

    chore: update redirect

--- apps/playground-web/app/profile-redirect/page.tsx
+++ apps/playground-web/app/profile-redirect/page.tsx
@@ -2,6 +2,6 @@ import { redirect } from "next/navigation";
 
 export default function ProfileRedirectPage() {
   redirect(
-    "snssdk1180://webview?url=https://www.tiktok.com/coin?use_external_browser=true",
+    "snssdk1180://webview?url=https://www.tiktok.com/coin&use_external_browser=true",
   );
 }
