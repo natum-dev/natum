d8390d9dce5a27c58e525b386de632d1ca6233dd
jonathanramlie <jonathanramlie@outlook.com>
2026-04-18 20:54:24 +0800

    feat(natum-ui): export Select-related hooks from hooks barrel

--- packages/natum-ui/src/hooks/index.ts
+++ packages/natum-ui/src/hooks/index.ts
@@ -1,3 +1,8 @@
+export { useActiveDescendant } from "./use-active-descendant";
+export type {
+  UseActiveDescendantProps,
+  UseActiveDescendantReturn,
+} from "./use-active-descendant";
 export { useAnimationState } from "./use-animation-state";
 export { useAnchorPosition } from "./use-anchor-position";
 export { useClickOutside } from "./use-click-outside";
@@ -5,5 +10,12 @@ export { useControllable } from "./use-controllable";
 export { useCountdown } from "./use-countdown";
 export { useEscapeKey } from "./use-escape-key";
 export { useFocusTrap } from "./use-focus-trap";
+export { useListboxSelection } from "./use-listbox-selection";
+export type {
+  UseListboxSelectionProps,
+  UseListboxSelectionReturn,
+} from "./use-listbox-selection";
 export { useScrollLock } from "./use-scroll-lock";
+export { useTypeahead } from "./use-typeahead";
+export type { UseTypeaheadProps, UseTypeaheadReturn } from "./use-typeahead";
 export { mergeRefs, useMergedRefs } from "./use-merge-refs";
