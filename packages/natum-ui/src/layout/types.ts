9de1c7f9ce9e0da2ed1f23b8be25e0b0856f24aa
jonathanramlie <jonathanramlie@outlook.com>
2026-04-18 16:30:57 +0800

    feat(natum-ui): add shared layout types (Space, Align, Justify)

---
+++ packages/natum-ui/src/layout/types.ts
@@ -0,0 +1,11 @@
+export type Space = 0 | 1 | 2 | 3 | 4 | 6 | 8 | 12 | 16;
+
+export type Align = "start" | "center" | "end" | "stretch" | "baseline";
+
+export type Justify =
+  | "start"
+  | "center"
+  | "end"
+  | "between"
+  | "around"
+  | "evenly";
