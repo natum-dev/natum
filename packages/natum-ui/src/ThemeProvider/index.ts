d6aa1b3d412d48e328fc576ff05069a9bd47b8f8
jonathanramlie <jonathanramlie@outlook.com>
2026-04-18 15:25:02 +0800

    feat(theme): export ThemeScript, ResolvedTheme, parseThemeCookie

--- packages/natum-ui/src/ThemeProvider/index.ts
+++ packages/natum-ui/src/ThemeProvider/index.ts
@@ -1,5 +1,7 @@
 export { ThemeProvider } from "./ThemeProvider";
 export type { ThemeProviderProps } from "./ThemeProvider";
+export { ThemeScript } from "./ThemeScript";
 export { useTheme } from "./useTheme";
 export { ThemeContext } from "./ThemeContext";
-export type { Theme, ThemeContextValue } from "./ThemeContext";
+export type { Theme, ResolvedTheme, ThemeContextValue } from "./ThemeContext";
+export { parseThemeCookie, THEME_COOKIE_NAME } from "./cookie";
