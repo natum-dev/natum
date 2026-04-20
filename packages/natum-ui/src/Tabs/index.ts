a903d112904c1f22b6a183bc2348966013f99ec0
jonathanramlie <jonathanramlie@outlook.com>
2026-04-20 23:50:05 +0800

    feat(tabs): TabsPanel with lazy mount + keep-mounted

--- packages/natum-ui/src/Tabs/index.ts
+++ packages/natum-ui/src/Tabs/index.ts
@@ -5,3 +5,5 @@ export { TabsList } from "./TabsList";
 export type { TabsListProps } from "./TabsList";
 export { TabsTrigger } from "./TabsTrigger";
 export type { TabsTriggerProps } from "./TabsTrigger";
+export { TabsPanel } from "./TabsPanel";
+export type { TabsPanelProps } from "./TabsPanel";
