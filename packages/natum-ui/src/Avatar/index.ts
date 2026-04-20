f6e6c72cf3d6bae6b9f263c5d473a83badf654fb
jonathanramlie <jonathanramlie@outlook.com>
2026-04-21 00:06:44 +0800

    feat(avatar): AvatarGroup scaffold + overflow chip

--- packages/natum-ui/src/Avatar/index.ts
+++ packages/natum-ui/src/Avatar/index.ts
@@ -1,2 +1,4 @@
 export { Avatar } from "./Avatar";
 export type { AvatarProps, AvatarSize, AvatarShape, AvatarColor } from "./Avatar";
+export { AvatarGroup } from "./AvatarGroup";
+export type { AvatarGroupProps } from "./AvatarGroup";
